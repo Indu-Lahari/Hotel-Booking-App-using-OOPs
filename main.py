bbf1f35287ba4310165e233aeabd00886b1e87b4
Indu <indulahari6@gmail.com>
2024-07-24 19:32:37 +0530

    Create Classes

---
+++ main.py
@@ -0,0 +1,15 @@
+class User:
+    pass
+
+
+class Hotel:
+    def view_hotels(self):
+        pass
+
+    def book(self):
+        pass
+
+
+class ReservationTicket:
+    def generate(self):
+        pass
